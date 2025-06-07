b64e4e8a9dbc22fdac1760c3f5908b4283f8d933
Switchbreak <switch@switchbreak.net>
2025-06-07 03:52:45 -0700

    Moving lobby chat to WebRTC peer to peer
    Also adding WebRTC native extension

--- lobby.gd
+++ lobby.gd
@@ -9,6 +9,8 @@ signal server_disconnected
 @onready var send_chat_button := $SendChatMessage
 @onready var signal_server := $SignalServer
 
+const DEFAULT_STUN_SERVER: String = "stun:stun.l.google.com:19302"
+
 var matchmaking_socket := WebSocketPeer.new()
 var prev_state: WebSocketPeer.State
 
@@ -63,12 +65,24 @@ func _matchmaking_server_disconnected() -> void:
     printerr("Disconnected from matchmaking server: %d - %s" % [code, reason])
 
 
+func _matchmaking_server_send(type: int, data: String, index: int = -1) -> void:
+    matchmaking_socket.send_text(JSON.stringify({
+        "type": type,
+        "data": data,
+        "peer_index": index,
+    }))
+
+
 func _poll_matchmaking_server() -> void:
     matchmaking_socket.poll()
 
     var state := matchmaking_socket.get_ready_state()
     if state == WebSocketPeer.STATE_OPEN:
         _receive_matchmaking_messages()
+        if state != prev_state:
+            # When connection first opens, send notification to matchmaking server
+            _matchmaking_server_send(signal_server.Message.PEER_CONNECT, TableValues.player_info.name)
+
     elif state == WebSocketPeer.STATE_CLOSED and state != prev_state:
         _matchmaking_server_disconnected()
 
@@ -91,7 +105,36 @@ func _receive_matchmaking_messages() -> Error:
 
 
 func _handle_matchmaking_message(packet: String) -> Error:
-    print("Received packet from matchmaking server: " + packet)
+    print(TableValues.player_info.name + ": Received packet from matchmaking server: " + packet)
+
+    var message: Dictionary = JSON.parse_string(packet)
+    var peer_index := int(message.peer_index)
+
+    match int(message.type):
+        signal_server.Message.SET_ID:
+            init_rtc(peer_index)
+        signal_server.Message.PEER_CONNECT:
+            _register_player({"name": message.data}, peer_index)
+            _connect_peer(peer_index)
+        signal_server.Message.PEER_DISCONNECT:
+            _on_player_disconnected(peer_index)
+        signal_server.Message.OFFER:
+            var rtc_peer: WebRTCMultiplayerPeer = multiplayer.multiplayer_peer
+            print(TableValues.player_info.name + ": Offer received")
+            if rtc_peer.has_peer(peer_index):
+                rtc_peer.get_peer(peer_index).connection.set_remote_description("offer", message.data)
+        signal_server.Message.ANSWER:
+            var rtc_peer: WebRTCMultiplayerPeer = multiplayer.multiplayer_peer
+            print(TableValues.player_info.name + ": Answer received")
+            if rtc_peer.has_peer(peer_index):
+                rtc_peer.get_peer(peer_index).connection.set_remote_description("answer", message.data)
+        signal_server.Message.CANDIDATE:
+            var rtc_peer: WebRTCMultiplayerPeer = multiplayer.multiplayer_peer
+            print(TableValues.player_info.name + ": Candidate received")
+            var candidate: PackedStringArray = message.data.split("\n", false)
+            if rtc_peer.has_peer(peer_index):
+                rtc_peer.get_peer(peer_index).connection.add_ice_candidate(candidate[0], candidate[1].to_int(), candidate[2])
+
     return OK
 
 #endregion
@@ -102,7 +145,9 @@ func create_table() -> Error:
     if error:
         return error
 
-    return await join_table(TableValues.DEFAULT_SERVER_IP, TableValues.DEFAULT_PORT)
+    TableValues.player_info.is_host = true
+
+    return join_table(TableValues.DEFAULT_SERVER_IP, TableValues.DEFAULT_PORT)
 
     #var peer = WebRTCMultiplayerPeer.new()
     #var error = peer.create_server()
@@ -129,9 +174,6 @@ func join_table(address: String = "", port: int = TableValues.DEFAULT_PORT) -> E
     if error:
         return error
 
-    await get_tree().create_timer(1).timeout
-    matchmaking_socket.send_text("Hello from " + TableValues.player_info.name)
-
     return OK
 
     #if address.is_empty():
@@ -147,8 +189,23 @@ func join_table(address: String = "", port: int = TableValues.DEFAULT_PORT) -> E
     #_init_lobby()
 
 
+func init_rtc(index: int) -> Error:
+    var peer := WebRTCMultiplayerPeer.new()
+    var error := peer.create_mesh(index)
+    if error:
+        return error
+    multiplayer.multiplayer_peer = peer
+
+    TableValues.players[index] = TableValues.player_info
+    player_connected.emit(index)
+
+    return OK
+
+
 func remove_multiplayer_peer():
-    multiplayer.multiplayer_peer = null
+    if multiplayer.multiplayer_peer:
+        multiplayer.multiplayer_peer.close()
+        multiplayer.multiplayer_peer = null
     TableValues.players.clear()
     show_chat_message("Disconnected from lobby")
 
@@ -172,21 +229,57 @@ func send_chat_message(message: String, sender_player_info = TableValues.player_
 
 
 @rpc("any_peer", "reliable")
-func _register_player(new_player_info):
-    var new_player_id := multiplayer.get_remote_sender_id()
+func _register_player(new_player_info, index) -> void:
+    #var new_player_id := multiplayer.get_remote_sender_id()
     new_player_info.is_self = false
-    TableValues.players[new_player_id] = new_player_info
-    player_connected.emit(new_player_id)
+    new_player_info.is_host = false
+    TableValues.players[index] = new_player_info
+    player_connected.emit(index)
     if multiplayer.is_server():
         send_chat_message("%s has entered the lobby" % new_player_info.name, { "name": "Server" })
 
 
-func _on_player_connected(id: int):
-    _register_player.rpc_id(id, TableValues.player_info)
+func _connect_peer(index: int) -> Error:
+    var peer := WebRTCPeerConnection.new()
+    var rtc_peer: WebRTCMultiplayerPeer = multiplayer.multiplayer_peer
+
+    peer.initialize({
+        "iceServers": [ { "urls": [DEFAULT_STUN_SERVER] } ],
+    })
+    peer.session_description_created.connect(_offer_created.bind(index))
+    peer.ice_candidate_created.connect(_candidate_created.bind(index))
+    rtc_peer.add_peer(peer, index)
+    if not TableValues.player_info.is_host:
+        peer.create_offer()
+
+    return OK
+
+
+func _offer_created(type: String, data: String, index: int) -> void:
+    print(TableValues.player_info.name + ": Offer created")
+    var rtc_peer: WebRTCMultiplayerPeer = multiplayer.multiplayer_peer
+    rtc_peer.get_peer(index).connection.set_local_description(type, data)
+    if type == "offer":
+        _matchmaking_server_send(signal_server.Message.OFFER, data, index)
+    else:
+        _matchmaking_server_send(signal_server.Message.ANSWER, data, index)
+
+
+func _candidate_created(mid_name: String, index_name: int, sdp_name: String, index: int) -> void:
+    print(TableValues.player_info.name + ": Candidate created")
+    _matchmaking_server_send(signal_server.Message.CANDIDATE, "\n%s\n%d\n%s" % [mid_name, index_name, sdp_name], index)
+
+
+func _on_player_connected(_id: int):
+    #_register_player.rpc_id(id, TableValues.player_info)
+    pass
 
 
 func _on_player_disconnected(id: int):
-    if TableValues.players[id]:
+    var peer: WebRTCMultiplayerPeer = multiplayer.multiplayer_peer
+    if peer.has_peer(id):
+        peer.remove_peer(id)
+    if TableValues.players.has(id):
         show_chat_message("%s has left the lobby" % TableValues.players[id].name)
         TableValues.players.erase(id)
     player_disconnected.emit(id)
--- signal_server.gd
+++ signal_server.gd
@@ -1,7 +1,18 @@
 extends Node
 
+enum Message {
+    SET_ID,
+    PEER_CONNECT,
+    PEER_DISCONNECT,
+    OFFER,
+    ANSWER,
+    CANDIDATE,
+}
+
 class Peer extends RefCounted:
     var socket := WebSocketPeer.new()
+    var name := "Player"
+    var id_set := false
 
     func _init(stream_peer: StreamPeerTCP) -> void:
         socket.accept_stream(stream_peer)
@@ -10,7 +21,7 @@ class Peer extends RefCounted:
         return "%s:%d" % [socket.get_connected_host(), socket.get_connected_port()]
 
 var tcp_server := TCPServer.new()
-var peers: Array[Peer] = []
+var peers: Dictionary[int, Peer] = {}
 
 func _process(_delta: float) -> void:
     poll()
@@ -32,49 +43,63 @@ func poll() -> void:
     if tcp_server.is_connection_available():
         _connect_peer()
 
-    var disconnected: Array[Peer] = []
-    for peer in peers:
-        peer.socket.poll()
-        var state := peer.socket.get_ready_state()
+    var disconnected: Array[int] = []
+    for index in peers:
+        peers[index].socket.poll()
+        var state := peers[index].socket.get_ready_state()
         if state == WebSocketPeer.STATE_OPEN:
-            _receive_peer_messages(peer)
+            if not peers[index].id_set:
+                peers[index].socket.send_text(JSON.stringify({ "type": Message.SET_ID, "peer_index": index }))
+                peers[index].id_set = true
+            _receive_peer_messages(index)
         elif state == WebSocketPeer.STATE_CLOSED:
-            disconnected.append(peer)
+            disconnected.append(index)
 
     for peer in disconnected:
         _disconnect_peer(peer)
 
 func _connect_peer() -> void:
     var peer := Peer.new(tcp_server.take_connection())
-    peers.append(peer)
+    var index := randi() % (1 << 31)
+    peers[index] = peer
     print("Peer connected: " + peer.get_address())
 
-func _disconnect_peer(peer: Peer) -> void:
-    peers.erase(peer)
-    print("Peer disconnected: " + peer.get_address())
+func _disconnect_peer(index: int) -> void:
+    peers.erase(index)
+    print("Peer disconnected: %d" % index)
 
-func _receive_peer_messages(peer: Peer) -> Error:
-    while peer.socket.get_available_packet_count():
-        var packet := peer.socket.get_packet().get_string_from_utf8()
-        var error := peer.socket.get_packet_error()
+    for remaining_peer in peers:
+        # Send existing peers notification of disconnection
+        peers[remaining_peer].socket.send_text(JSON.stringify({ "type": Message.PEER_DISCONNECT, "peer_index": index, "data": "" }))
+
+
+func _receive_peer_messages(index: int) -> Error:
+    while peers[index].socket.get_available_packet_count():
+        var packet := peers[index].socket.get_packet().get_string_from_utf8()
+        var error := peers[index].socket.get_packet_error()
         if error:
-            printerr("Error receiving packet from peer %s - %s" % [peer.get_address(), error_string(error)])
+            printerr("Error receiving packet from peer %s - %s" % [peers[index].get_address(), error_string(error)])
             return error
 
-        error = _handle_peer_message(peer, packet)
+        error = _handle_peer_message(index, packet)
         if error:
             return error
 
     return OK
 
-func _handle_peer_message(peer: Peer, packet: String) -> Error:
-    print("Received packet from peer %s - %s" % [peer.get_address(), packet])
+func _handle_peer_message(from_index: int, packet: String) -> Error:
+    var message: Dictionary = JSON.parse_string(packet)
 
-    var response_timer := get_tree().create_timer(1)
-    response_timer.timeout.connect(_send_peer_response.bind(peer))
+    if message.type == Message.PEER_CONNECT:
+        peers[from_index].name = message.data
+        for index in peers:
+            if index != from_index:
+                # Send existing peers the index of the new peer
+                peers[index].socket.send_text(JSON.stringify({ "type": Message.PEER_CONNECT, "peer_index": from_index, "data": message.data }))
+                # Send the new peer a message for each existing peer
+                peers[from_index].socket.send_text(JSON.stringify({ "type": Message.PEER_CONNECT, "peer_index": index, "data": peers[index].name }))
+    else:
+        var destination := peers[int(message.peer_index)]
+        destination.socket.send_text(JSON.stringify({ "type": message.type, "peer_index": from_index, "data": message.data }))
 
     return OK
-
-func _send_peer_response(peer: Peer) -> void:
-    peer.socket.send_text("Hello from matchmaking server")
-    #stop()
