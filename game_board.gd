cae57812848b1475278075801729055fa55a78eb
Switchbreak <switch@switchbreak.net>
2025-06-09 20:25:55 -0700

    Rename board_scene to table_scene

--- game_board.gd
+++ game_board.gd
@@ -3,11 +3,10 @@ extends StaticBody3D
 @export var piece_scene: PackedScene
 @export var drag_location: Vector3
 
-@onready var scene := $"/root/BoardScene"
+@onready var scene := $"/root/TableScene"
 @onready var drag_surface := $"BoardDragSurface/CollisionShape3D"
 
 var drag_object: Node3D
-
 
 func toggle_drag(drag: bool, set_drag_object: Node3D = null) -> void:
     drag_surface.disabled = !drag
@@ -17,14 +16,12 @@ func toggle_drag(drag: bool, set_drag_object: Node3D = null) -> void:
     if drag_object != null:
         drag_object.toggle_drag(drag)
 
-
 func _on_input_event(_camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
     if event is InputEventMouseButton:
         if event.button_index == MouseButton.MOUSE_BUTTON_RIGHT && event.pressed:
             var piece = piece_scene.instantiate()
             piece.initialize(event_position)
             scene.add_child(piece)
-
 
 func _on_board_drag_surface_input_event(_camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
     if event is InputEventMouseMotion:
--- game_piece.gd
+++ game_piece.gd
@@ -1,24 +1,19 @@
 extends RigidBody3D
 
-
 @export var dragging: bool = false
-@onready var board: Node3D = $"/root/BoardScene/GameBoard"
-
+@onready var board: Node3D = $"/root/TableScene/GameBoard"
 
 func initialize(start_position: Vector3) -> void:
     position = start_position
-
 
 func toggle_drag(set_dragging: bool) -> void:
     dragging = set_dragging
     input_ray_pickable = !dragging
 
-
 func _on_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
     if event is InputEventMouseButton:
         if event.button_index == MouseButton.MOUSE_BUTTON_LEFT:
             board.toggle_drag(true, self)
-
 
 func _physics_process(_delta: float) -> void:
     if dragging:
